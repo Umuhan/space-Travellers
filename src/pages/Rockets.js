0b95fc3ffc69fe4b1b48863f62aa519087560971
Umu <hanny.ally123@gmail.com>
2023-11-14 17:16:47 +0300

    Modify Rockets.js file

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,22 +1,50 @@
-// src/pages/Rockets.js
 import React, { useEffect } from 'react';
 import '../components/styles/Rockets.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../redux/rockets/rocketsSlice';
+import { fetchRockets, reserveRocket } from '../redux/rockets/rocketsSlice';
 
 const Rockets = () => {
   const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rockets);
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const status = useSelector((state) => state.rockets.status);
+  const error = useSelector((state) => state.rockets.error);
 
   useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, status]);
+
+  const handleReserveRocket = (rocketId) => {
+    dispatch(reserveRocket(rocketId));
+  };
 
   return (
     <div>
-      {rockets.map((rocket) => (
-        <div key={rocket.id}>{rocket.name}</div>
-      ))}
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'failed' && (
+        <p>
+          Error loading rockets:
+          {error}
+        </p>
+      )}
+      {status === 'succeeded' && (
+        <div>
+          <h2>Rockets</h2>
+          <ul>
+            {rockets.map((rocket) => (
+              <li key={rocket.id}>
+                <h3>{rocket.rocket_name}</h3>
+                <p>{rocket.description}</p>
+                <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+                <button onClick={() => handleReserveRocket(rocket.id)}>
+                  Reserve Rocket
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
