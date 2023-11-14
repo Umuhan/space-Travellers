d00b75aa7fad5a78f1917bfd06f70ccd464c54d0
Umu <hanny.ally123@gmail.com>
2023-11-14 17:15:52 +0300

    Add provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 // src/App.js
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Rockets from "./pages/Rockets";
-import Missions from "./pages/Missions";
-import MyProfile from "./pages/MyProfile";
-import { Provider } from "react-redux";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navbar from './components/Navbar';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import MyProfile from './pages/MyProfile';
 
 const App = () => (
-  <Router>
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/rockets" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/myprofile" element={MyProfile} />
-      </Routes>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/rockets" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={MyProfile} />
+        </Routes>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
