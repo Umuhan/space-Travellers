0e27628afd7a67b671051f36306912af46cb2f0a
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-14 23:32:15 +0500

    Add actions and reducers for Joining and Leaving Missions

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,11 +1,29 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const API_URL = 'https://api.spacexdata.com/v3/missions';
+
 export const fetchMissions = createAsyncThunk(
   'missions/fetchMissions',
   async () => {
-    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    const response = await axios.get(API_URL);
     console.log(response.data);
+    return response.data;
+  },
+);
+
+export const joinMission = createAsyncThunk(
+  'missions/joinMission',
+  async (missionId) => {
+    const response = await axios.post(`${API_URL}/${missionId}/join`);
+    return response.data;
+  },
+);
+
+export const leaveMission = createAsyncThunk(
+  'missions/leaveMission',
+  async (missionId) => {
+    const response = await axios.post(`${API_URL}/${missionId}/leave`);
     return response.data;
   },
 );
@@ -31,6 +49,20 @@ const missionsSlice = createSlice({
         state.status = 'failed';
         state.error = action.error.message;
       });
+
+    builder.addCase(joinMission.fulfilled, (state, action) => {
+      const joinedMissionId = action.payload.mission_id;
+      state.missions = state.missions.map((mission) => (mission.mission_id === joinedMissionId
+        ? { ...mission, reserved: true }
+        : mission));
+    });
+
+    builder.addCase(leaveMission.fulfilled, (state, action) => {
+      const leftMissionId = action.payload.mission_id;
+      state.missions = state.missions.map((mission) => (mission.mission_id === leftMissionId
+        ? { ...mission, reserved: false }
+        : mission));
+    });
   },
 });
 
