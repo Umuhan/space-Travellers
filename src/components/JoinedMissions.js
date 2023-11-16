4099fa0b265903fecd93b6a433e56eb97414fe58
Umu <hanny.ally123@gmail.com>
2023-11-16 17:39:06 +0300

    Add table to myprofile file

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -1,32 +1,25 @@
 import { useSelector } from "react-redux";
-import myprofile from "./styles/myprofile.css";
-
+// import myprofile from './styles/myprofile.css';
 const JoinedMissions = () => {
   const { missions } = useSelector((state) => state.missions);
-
   const reservedMissions = missions.filter((mission) => mission.reserved);
-
+  const joinedMissionsStyling = {
+    border: reservedMissions.length === 0 ? "none" : "1px solid #E3E3E3",
+    marginTop: "10px",
+  };
   return (
-    <div>
-      <div>
-        <table className="mission-table">
-          <thead>
-            <tr>
-              <th>My Missions</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {reservedMissions.map(({ missionName, missionId }) => (
-              <tr key={missionId}>
-                <td>{missionName}</td>
-                {/* <td>{rocketId}</td> */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <>
+      {reservedMissions.length === 0 && (
+        <p className="no-mission">No Missions reserved...</p>
+      )}
+      <ul style={joinedMissionsStyling}>
+        {reservedMissions.map(({ missionName, missionId }) => (
+          <li key={missionId} className="item">
+            {missionName}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 export default JoinedMissions;
