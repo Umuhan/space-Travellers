182c89861fb20b3f5414292be8b2f73dab1777ea
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-16 15:23:17 +0500

    Render list of joined missions

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -1,2 +1,22 @@
-const JoinedMissions = () => <h2>JoinedMissions</h2>;
+import { useSelector } from 'react-redux';
+
+const JoinedMissions = () => {
+  const { missions } = useSelector((state) => state.missions);
+
+  const reservedMissions = missions.filter((mission) => mission.reserved);
+
+  return (
+    <div>
+      <div>
+        <h3>My Missions</h3>
+        {reservedMissions.length === 0 && <p>No reserved missions</p>}
+        <ul>
+          {reservedMissions.map(({ missionName, missionId }) => (
+            <li key={missionId}>{missionName}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default JoinedMissions;
