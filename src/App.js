b33fd8c6219e723e6e7bc7b50bcdfa26b85eeb40
Umu <hanny.ally123@gmail.com>
2023-11-16 19:21:48 +0300

    Fix eslint error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Navbar from "./components/Navbar";
-import Rockets from "./pages/Rockets";
-import Missions from "./pages/Missions";
-import MyProfile from "./pages/MyProfile";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navbar from './components/Navbar';
+import Rockets from './pages/Rockets';
+import Missions from './pages/Missions';
+import MyProfile from './pages/MyProfile';
 
 const App = () => (
   <Provider store={store}>
