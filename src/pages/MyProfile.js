af2ec09a3960ef56650cacd80604d29d085ab15d
Umu <hanny.ally123@gmail.com>
2023-11-16 17:41:23 +0300

    Modify MyProfile.js file

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "../components/styles/myprofile.css";
 import JoinedMissions from "../components/JoinedMissions";
 import ReservedRockets from "../components/ReservedRockets";
-
-const MyProfile = () => (
-  <div>
-    <JoinedMissions />;
-    <ReservedRockets />
-  </div>
-);
+const MyProfile = () => {
+  return (
+    <div className="profile-container">
+      <div className="rockets-container">
+        <h2>My Rockets</h2>
+        <ReservedRockets />
+      </div>
+      <div className="mission-container">
+        <h2>My Missions</h2>
+        <JoinedMissions />
+      </div>
+    </div>
+  );
+};
 export default MyProfile;
